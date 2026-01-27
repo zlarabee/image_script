0afb364932e37160f5f01fec1d334a9439070809
Zach Larabee <zachlarabee@gmail.com>
2026-01-27 16:29:18 -0500

    fully refactored and tested using pathlib

--- image_script.py
+++ image_script.py
@@ -5,43 +5,56 @@ import shutil
 
 from pathlib import Path
 
-copy_from_directory_path = Path("/volumes/RICOH GR/DCIM/100RICOH")
+# copy_from_directory_path = Path("/volumes/RICOH GR/DCIM/100RICOH")
+copy_from_directory_path = Path("/Users/zachlarabee/pictures/test")
 base_copy_to_directory_path = Path("/Users/zachlarabee/pictures")
 
 def rename_files_with_creation_date(copy_from_directory, base_copy_to_directory):
 	
-	for file in os.listdir(copy_from_directory):
-		# source = os.path.join(copy_from_directory, file)
-		source = copy_from_directory / file
-		# this check is needed in order to ignore directories
+	created_dirs = set()
+	
+	for image in copy_from_directory.iterdir():
 
-		if source.is_file():
+		if image.is_file():
 			
 			# get file extension for use later
-			file_extension = source.suffix.lower()
+			file_extension = image.suffix.lower()
 	
 			# next line is not readable as a date by humans
-			creation_time = os.path.getctime(source)
+			creation_time = os.path.getctime(image)
 
 			# make timestamp readable
 			date_created = datetime.datetime.fromtimestamp(creation_time)
 			date_folder = date_created.strftime('%Y_%m')
 
-			target_directory = base_copy_to_directory_path / date_folder
+			jpg_directory = base_copy_to_directory_path / date_folder
+			raw_file_sub_directory = jpg_directory / 'raw'
 
-			# subfolder for raw files
-			raw_file_sub_directory = target_directory / 'raw'
-			Path.mkdir(target_directory, exist_ok=True)
-			Path.mkdir(raw_file_sub_directory, exist_ok=True)
-			# os.makedirs(target_directory, exist_ok=True)
-			# os.makedirs(raw_file_sub_directory, exist_ok=True)
+			# create dirs if they don't exist
+			if date_folder not in created_dirs:
+				# make dirs
+				Path.mkdir(jpg_directory, exist_ok=True)
 
-			new_filename = f"{date_created.strftime('%Y_%m_%d')}_{file}"
-			destination = target_directory / new_filename
-			# destination = os.path.join(target_directory, new_filename)
+				# create raw subdir				
+				Path.mkdir(raw_file_sub_directory, exist_ok=True)
 
-			shutil.copy2(source, destination)
-			print(f"Copied '{file}' to '{destination}'")
+				created_dirs.add(date_folder)
+				print(f"created directories {jpg_directory} and {raw_file_sub_directory}")
+
+			new_filename = f"{date_created.strftime('%Y_%m_%d')}_{image.name}"
+
+			if file_extension == ".jpg" or file_extension == ".jpeg":
+
+				destination = jpg_directory / new_filename
+				# destination = os.path.join(jpg_directory, new_filename)
+
+				shutil.copy2(image, destination)
+			else:
+				destination = raw_file_sub_directory / new_filename
+				# destination = os.path.join(jpg_directory, new_filename)
+
+				shutil.copy2(image, destination)
+			print(f"Copied '{image}' to '{destination}'")
 
 
 
